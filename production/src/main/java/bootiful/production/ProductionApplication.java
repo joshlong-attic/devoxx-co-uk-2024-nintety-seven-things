905161ab520f5ed65ef77854582d8fe5eff56ac0
Josh Long <josh@joshlong.com>
2024-05-08 14:33:46 +0100

    polish

--- production/src/main/java/bootiful/production/ProductionApplication.java
+++ production/src/main/java/bootiful/production/ProductionApplication.java
@@ -1,16 +1,16 @@
 package bootiful.production;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
-
-import static org.springframework.web.servlet.function.RouterFunctions.route;
-import static org.springframework.web.servlet.function.ServerResponse.*;
 
 @SpringBootApplication
 public class ProductionApplication {
@@ -20,28 +20,33 @@ public class ProductionApplication {
     }
 
     @Bean
-    @LoadBalanced
-    RestClient.Builder builder() {
-        return RestClient.builder();
-    }
-
-
-
-    @Bean
-    RouterFunction<ServerResponse> routes(RestClient.Builder builder) {
-        var restClient = builder.build() ;
-        return route()
-                .GET("/{seconds}", request -> {
-                    var body = restClient
-                            .get()
-                            .uri("http://service/hello")
-                            .retrieve()
-                            .body(String.class);
-
-                    return ok().body(body);
-                })
-                .build();
-
+    RestClient http() {
+        return RestClient.builder().build();
     }
 }
 
+@Controller
+@ResponseBody
+class ReliableController {
+
+    private final CircuitBreaker cb;
+
+    private final RestClient http;
+
+    ReliableController(RestClient http, CircuitBreakerFactory<?,?> circuitBreakerFactory) {
+        this.cb = circuitBreakerFactory.create("try");
+        this.http = http;
+    }
+
+    @GetMapping("/try")
+    ResponseEntity<?> call() {
+        return this.cb.run(() -> this.http
+                        .get()
+                        .uri("https://httpbingo.org/unstable")
+                        .retrieve()
+                        .toEntity(new ParameterizedTypeReference<>() {
+                        }),
+                throwable -> ResponseEntity.notFound().build());
+    }
+
+}
--- service/src/main/java/com/example/service/ServiceApplication.java
+++ service/src/main/java/com/example/service/ServiceApplication.java
@@ -3,17 +3,16 @@ package com.example.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static org.springframework.web.servlet.function.RouterFunctions.route;
-import static org.springframework.web.servlet.function.ServerResponse.ok;
-
+@Controller
+@ResponseBody
 @SpringBootApplication
 public class ServiceApplication {
 
@@ -28,10 +27,9 @@ public class ServiceApplication {
         this.port.set(serverInitializedEvent.getWebServer().getPort());
     }
 
-    @Bean
-    RouterFunction<ServerResponse> get() {
-        return route()
-                .GET("/hello", request -> ok().body(Map.of("message", "Hello from port " + port.get())))
-                .build();
+    @GetMapping("/hello")
+    Map<String, String> hello() {
+        return Map.of("message", "Hello from port " + port.get());
     }
+
 }
